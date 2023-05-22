e84711fdae59f5c89f4e98c2ca30645cf529b335
Eric <ericstumper@users.noreply.github.com>
2023-05-22 19:03:19 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+  plugins: [react()],
+});
